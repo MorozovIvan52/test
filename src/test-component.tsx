9ff25cbb824c172acb0984ec5627ad69d9bdfcdd
ivan <kipelov.ivan152@gmail.com>
2022-06-20 21:37:17 +0300

    add component

--- src/test-component.tsx
+++ src/test-component.tsx
@@ -1,3 +1,15 @@
+import * as React from 'react';
 
+type NewComponentProps = {};
+export const NewComponent = (props: NewComponentProps) => {
+    return (
+        <div>
+            <div>
+               i dmitriy yulin
+            </div>
+
+        </div>
+    );
+};
 
 
