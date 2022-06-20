78e73b4abc4353b6476ccd3b22adde54aeefecc7
ivan <kipelov.ivan152@gmail.com>
2022-06-20 21:39:24 +0300

    fix test component

--- src/test-component.tsx
+++ src/test-component.tsx
@@ -1,15 +1,3 @@
-import * as React from 'react';
-
-type NewComponentProps = {};
-export const NewComponent = (props: NewComponentProps) => {
-    return (
-        <div>
-            <div>
-               i dmitriy yulin
-            </div>
-
-        </div>
-    );
-};
+import * as React from 'react'
 
 
